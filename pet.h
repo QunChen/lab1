f1fc5b5109dee55e7483cb5a1a809e3942b50c55
QunChen <qunchen1986@gmail.com>
2016-03-05 16:52:36 +1100

    create struct

---
+++ pet.h
@@ -0,0 +1,27 @@
+//
+// Created by think on 2016/3/5.
+//
+
+
+#ifndef LAB1_PET_H
+#define LAB1_PET_H
+
+#include <string>
+using namespace std;
+struct Pet{
+        string status;
+        string pet_type;
+        string gender;
+        string breeds;
+        string age_of_year;
+        string age_of_month;
+        string colour;
+        string chip_number;
+        string location;
+        string contact_phone_number;
+};
+
+
+
+
+#endif //LAB1_PET_H
